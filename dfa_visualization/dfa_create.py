a327830dfbd01b2e3310d54f927511e801f31b65
spirosmaggioros <51701672+spirosmaggioros@users.noreply.github.com>
2022-11-15 19:47:53 +0200

    Update dfa_create.py

--- dfa_visualization/dfa_create.py
+++ dfa_visualization/dfa_create.py
@@ -1,3 +1,4 @@
+print("App by Maggioros Spiros")
 import os
 import sys
 sys.path
@@ -6,58 +7,59 @@ from automathon import DFA
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-Q = set(input("Πληκτρολογήστε τις καταστάσεις με κενό ενδοιάμεσα και έπειτα πατήστε εντερ\n").split())
+def dfa_creation():
+    Q = set(input("Πληκτρολογήστε τις καταστάσεις με κενό ενδιάμεσα και έπειτα πατήστε εντερ\n").split())
 
-Q = sorted(Q)
+    Q = sorted(Q)
 
-print(Q)
+    print(Q)
 
-sigma = set(input("πληκτρολογήστε την γλώσσα με κενό ενδοιάμεσα απο κάθε χαραχτήρα και έπειτα πατήστε εντερ\n").split())
+    sigma = set(input("πληκτρολογήστε τα στοιχεία του αλφαβήτου με κενό ενδιάμεσα απο κάθε χαραχτήρα και έπειτα πατήστε εντερ\n").split())
 
-sigma = sorted(sigma)
+    sigma = sorted(sigma)
 
-print(sigma)
-delta = {}
+    print(sigma)
+    delta = {}
 
-temp_1 = list(Q)
-temp_2 = list(sigma)
+    temp_1 = list(Q)
+    temp_2 = list(sigma)
 
-print("Δώστε τα transitions με την σειρά που έχετε στον πίνακα καταστάσεων σας\n") 
-# π.χ έστω ο παρακάτω πίνακας
-#      a       b
-# q0  q1       q2
-# q1  q3       q0
-# q2  q4       q1 
-# q3  q0       q2
-#
-# εμέις θα πρέπει με την σειρά(Για ευκολία) να δώσουμε 
-# q1 q2 q3 q0 q4 .... q2(με εντερ μετα απο κάθε κατάσταση λόγω του ότι η python δεν υποστηρίζει λειτουργίες όπως του cin της c++)
-#
+    print("Δώστε τα transitions με την σειρά που έχετε στον πίνακα καταστάσεων σας\n") 
+    print("# π.χ έστω ο παρακάτω πίνακας")
+    print("#      a       b")
+    print("# q0  q1       q2")
+    print("# q1  q3       q0")
+    print("# q2  q4       q1") 
+    print("# q3  q0       q2")
+    print("#")
+    print("# εμέις θα πρέπει με την σειρά(Για ευκολία) να δώσουμε")
+    print("# q1 q2 q3 q0 q4 .... q2(με εντερ μετα απο κάθε κατάσταση λόγω του ότι η python δεν υποστηρίζει λειτουργίες όπως του cin της c++)")
+    print("#")
 
-for i in range(len(temp_1)):
-    foo = {}
-    for j in range(len(temp_2)):
-        uio = input()
-        foo[temp_2[j]] = uio
-    delta[temp_1[i]] = foo
+    for i in range(len(temp_1)):
+        foo = {}
+        for j in range(len(temp_2)):
+            uio = input()
+            foo[temp_2[j]] = uio
+        delta[temp_1[i]] = foo
 
-pro = input("Δώστε την αρχική κατάσταση , :")
-initialState = pro
+    pro = input("Δώστε την αρχική κατάσταση , :")
+    initialState = pro
 
-F = set(input("Πληκτρολογήστε τις τελικές καταστάσεις με κενό ενδοιάμεσα και έπειτα πατήστε εντερ\n").split())
+    F = set(input("Πληκτρολογήστε τις τελικές καταστάσεις με κενό ενδιάμεσα και έπειτα πατήστε εντερ\n").split())
 
-automata1 = DFA(Q, sigma , delta , initialState, F)
+    automata1 = DFA(Q, sigma , delta , initialState, F)
 
-#print(automata1.isValid())#προφανώς επιστρέφει True
+    #print(automata1.isValid())#προφανώς επιστρέφει True
 
-#μπορούμε επίσης να ελέγξουμε καταστάσεις
-#print(automata1.accept("001010"))#True αν δέχεται , False αλλιώς
- 
-#μπορούμε να πάρουμε και το συμπλήρωμα μιάς γλώσσας ώς εξής
-#automata1.complement()
+    #μπορούμε επίσης να ελέγξουμε καταστάσεις
+    #print(automata1.accept("001010"))#True αν δέχεται , False αλλιώς
 
-automata1.view("MyDFA")#graph του DFA
+    #μπορούμε να πάρουμε και το συμπλήρωμα μιάς γλώσσας ώς εξής
+    #automata1.complement()
 
-img = mpimg.imread('MyDFA.gv.png')
-imgplot = plt.imshow(img)
-plt.show()
+    automata1.view("MyDFA")#graph του DFA
+
+    img = mpimg.imread('MyDFA.gv.png')
+    imgplot = plt.imshow(img)
+    plt.show()
