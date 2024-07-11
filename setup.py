ba8dd6e6e7c48e5a47929292ed3e375167ee6bc0
Spiros Maggioros <spirosastro@gmail.com>
2024-07-12 02:25:33 +0300

    Check release type

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 import codecs 
 import os
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'A basic DFA/NFA visualization tool only for homework purposes'
 
 setup(
     name="DFA-visual-demo",
     version=VERSION,
     author="Spiros Maggioros",
-    atuhor_email="<spirosmag@ieee.org>",
+    author_email="<spirosmag@ieee.org>",
     description=DESCRIPTION,
     packages=find_packages()
 )
