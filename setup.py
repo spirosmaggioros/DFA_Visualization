7e7b508bb0987cb5b11f6e985408ab0c9315f6ab
Spiros Maggioros <spirosastro@gmail.com>
2024-07-11 08:34:38 +0300

    Added twine

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import codecs 
+import os
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A basic DFA/NFA visualization tool only for homework purposes'
+
+setup(
+    name="DFA-visual-demo",
+    version=VERSION,
+    author="Spiros Maggioros",
+    atuhor_email="<spirosmag@ieee.org>",
+    description=DESCRIPTION,
+    packages=find_packages()
+)
